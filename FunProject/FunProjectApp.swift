ace3f62d84220d065a67811cfa8bf5c07927eff4
Wilda <wildaakmalia14@gmail.com>
2023-07-31 12:48:20 +0700

    Initial Commit

---
+++ FunProject/FunProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunProjectApp.swift
+//  FunProject
+//
+//  Created by Wilda Akmalia on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FunProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
